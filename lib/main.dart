1d3e6153ed33ffeaebe0c4b897cd7c306976bbc9
cckn <cckn1985@gmail.com>
2020-04-29 11:02:24 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        title: "Flutter Demo", theme: ThemeData.light(), home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Center"),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage("assets/sample.jpg"),
+          ),
+        ));
+  }
+}
