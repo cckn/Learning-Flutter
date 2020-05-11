d2cbcdc50b5d2696dfe889ef98c7ee2f697f69c8
cckn <cckn1985@gmail.com>
2020-05-11 20:12:10 +0900

    Make StopWatch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -18,60 +18,18 @@ class StopWatchPage extends StatefulWidget {
 }
 
 class _StopWatchPageState extends State<StopWatchPage> {
-  Widget _buildBody() {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 30),
-        child: Stack(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Row(
-                  // Show Time Field
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Text(
-                      "0",
-                      style: TextStyle(fontSize: 50),
-                    ),
-                    Text("00")
-                  ],
-                ),
-                Container(
-                  // Show LabTime Field
-                  width: 100,
-                  height: 200,
-                  child: ListView(
-                    children: <Widget>[],
-                  ),
-                )
-              ],
-            ),
-            Positioned(
-              left: 10,
-              bottom: 10,
-              child: FloatingActionButton(
-                backgroundColor: Colors.deepOrange,
-                onPressed: () {},
-                child: Icon(Icons.rotate_left),
-              ),
-            ),
-            Positioned(
-              right: 10,
-              bottom: 10,
-              child: RaisedButton(
-                onPressed: () {},
-                child: Text("랩타임"),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
+  Timer _timer;
 
-  void _clickButton() {}
+  num _time = 0;
+  bool _isRunning = false;
+
+  List<String> _lapTimes = [];
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -91,9 +49,106 @@ class _StopWatchPageState extends State<StopWatchPage> {
             _clickButton();
           });
         },
-        child: Icon(Icons.play_arrow),
+        child: _isRunning ? (Icon(Icons.pause)) : (Icon(Icons.play_arrow)),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+
+  Widget _buildBody() {
+    var sec = _time ~/ 100;
+    var hundredth = "${_time % 100}".padLeft(2, "0");
+
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 30),
+        child: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Row(
+                  // Show Time Field
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      "$sec",
+                      style: TextStyle(fontSize: 50),
+                    ),
+                    Text("$hundredth")
+                  ],
+                ),
+                Container(
+                  // Show LabTime Field
+                  width: 100,
+                  height: 200,
+                  child: ListView(
+                    children: _lapTimes.map((time) => Text(time)).toList(),
+                  ),
+                )
+              ],
+            ),
+            Positioned(
+              // Reset Button
+              left: 10,
+              bottom: 10,
+              child: FloatingActionButton(
+                backgroundColor: Colors.deepOrange,
+                onPressed: _reset,
+                child: Icon(Icons.rotate_left),
+              ),
+            ),
+            Positioned(
+              // LapTime Button
+              right: 10,
+              bottom: 10,
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    _recordLapTime("$sec.$hundredth");
+                  });
+                },
+                child: Text("랩타임"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _clickButton() {
+    _isRunning = !_isRunning;
+
+    if (_isRunning) {
+      _start();
+    } else {
+      _pause();
+    }
+  }
+
+  void _start() {
+    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+
+  void _pause() {
+    _timer?.cancel();
+  }
+
+  void _reset() {
+    setState(() {
+      _isRunning = false;
+      _timer?.cancel();
+      _lapTimes.clear();
+      _time = 0;
+    });
+  }
+
+  void _recordLapTime(String time) {
+    _lapTimes.add("${_lapTimes.length + 1}등 $time");
   }
 }
